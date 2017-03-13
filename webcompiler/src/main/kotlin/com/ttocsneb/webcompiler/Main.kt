84a5f74b3f7e983fd77e3e11f6e69dff81153a29
Benjamin Jacobs <contact@ttocsneb.com>
2017-03-12 21:51:41 -0600

    setup the json formats; load config.json

--- webcompiler/src/main/kotlin/com/ttocsneb/webcompiler/Main.kt
+++ webcompiler/src/main/kotlin/com/ttocsneb/webcompiler/Main.kt
@@ -1,13 +1,50 @@
 package com.ttocsneb.webcompiler
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.ttocsneb.webcompiler.json.JsonConfig
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileReader
+import java.io.FileWriter
+
 /**
  * This is the main entry point for the program, as well as most of the processing.
  */
 class Main {
 
+
     companion object {
+        val configFile:String = "config.json"
+
         @JvmStatic  fun main(args: Array<String>) {
-            println("Hello World!")
+
+            //Create a gson with pretty print enabled.
+            val gson:Gson = GsonBuilder().setPrettyPrinting().create()
+
+            //load the config file
+            val configReader = FileReader(File(configFile))
+            val config = gson.fromJson(configReader.readText(), JsonConfig::class.java)
+            configReader.close()
+
+            //Check if the template has changed
+            val templateReader = FileReader(File(config.template))
+            val template = templateReader.readText()
+            val changed = template.hashCode() != config.templatehash
+            if(changed) {
+                //modify the saved hash to the current hash
+                config.templatehash = template.hashCode()
+                val fw = FileWriter(configFile)
+                val bw = BufferedWriter(fw)
+                bw.write(gson.toJson(config))
+                bw.close()
+                fw.close()
+                println("The Template has changed, everything will be compiled..")
+            }
+
+            
+
+
         }
     }
 
---
+++ webcompiler/src/main/kotlin/com/ttocsneb/webcompiler/json/JsonCompiled.kt
@@ -0,0 +1,7 @@
+package com.ttocsneb.webcompiler.json
+
+/**
+ * This will be generated at 
+ */
+class JsonCompiled {
+}
