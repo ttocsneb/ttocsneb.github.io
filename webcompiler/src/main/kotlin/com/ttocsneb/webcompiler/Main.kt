9a71da9e11536bf804d6e68ff0213628054e463d
Benjamin Jacobs <contact@ttocsneb.com>
2017-03-12 18:07:29 -0600

    Create Java environment

---
+++ webcompiler/src/main/kotlin/com/ttocsneb/webcompiler/Main.kt
@@ -0,0 +1,14 @@
+package com.ttocsneb.webcompiler
+
+/**
+ * This is the main entry point for the program, as well as most of the processing.
+ */
+class Main {
+
+    companion object {
+        @JvmStatic  fun main(args: Array<String>) {
+            println("Hello World!")
+        }
+    }
+
+}
