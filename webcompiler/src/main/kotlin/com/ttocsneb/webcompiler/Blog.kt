0ffc9f3a6d64492f7db7d1024598160087c74e2f
Benjamin Jacobs <contact@ttocsneb.com>
2017-12-09 11:50:43 -0700

    Refactor (#2)
    * Format Main and Blog
    
    * Format Main Page and Rss
    
    * Forgot to compile

--- webcompiler/src/main/kotlin/com/ttocsneb/webcompiler/Blog.kt
+++ webcompiler/src/main/kotlin/com/ttocsneb/webcompiler/Blog.kt
@@ -13,7 +13,6 @@ import com.vladsch.flexmark.util.options.MutableDataSet
 import java.io.File
 import java.nio.file.Files
 import java.nio.file.StandardCopyOption
-import java.sql.Timestamp
 import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.*
@@ -22,6 +21,8 @@ import java.util.*
  * Compile Blog
  */
 class Blog {
+
+    val TEMPLATE_BLOG = "blog"
 
     val BLOG:String = "blog"
     val CAROUSEL:String = "carousel"
@@ -42,27 +43,22 @@ class Blog {
 
     /**
      * Compile the blogs
+     *
+     * @param args processed arguments
+     * @param config Global configuration
      */
-    fun compile(args: Main.args, config: JsonConfig) {
+    fun compile(args: Main.Args, config: JsonConfig) {
         println("Compiling Blogs..")
         val gson: Gson = GsonBuilder().setPrettyPrinting().create()
 
         //find the template for blogs
-        var t:JsonTemplate? = null
-        var templateFile:String = ""
-        for(f in Main.getFiles(config.template, "json")) {
-            val temp = gson.fromJson(Main.readFile(f), JsonTemplate::class.java)
-            if(temp.template == "blog") {
-                t = temp
-                templateFile = f
-                break
-            }
-        }
-        if(t == null) t = JsonTemplate()
-        val templateconfig = t
+        val templateFile: String = Main.getTemplate(config.template, TEMPLATE_BLOG, gson)!!
+        val templateConfig: JsonTemplate = gson.fromJson(Main.readFile(templateFile), JsonTemplate::class.java)
+        template = Main.readFile(File(templateFile).parentFile.path + "/" + templateConfig.file)
+
 
         //load the template tags
-        for(i in templateconfig.items) {
+        for(i in templateConfig.items) {
             when(i.name) {
                 BLOG -> blog = i.value
                 CAROUSEL -> carousel = i.value
@@ -73,26 +69,24 @@ class Blog {
                 AUTHOR -> author = i.value
             }
         }
-        template = Main.readFile(File(templateFile).parentFile.path + "/" + templateconfig.file)
-
 
 
         //check if everything needs to be compiled.
-        val changed = template.hashCode() != templateconfig.hash || config.featured.hashCode() != config.featuredhash || args.loadAll
+        val changed = template.hashCode() != templateConfig.hash || config.featured.hashCode() != config.featuredhash || args.loadAll
         if(changed) {
-            if(template.hashCode() != templateconfig.hash)println("Template has changed")
+            if(template.hashCode() != templateConfig.hash)println("Template has changed")
             if(config.featured.hashCode() != config.featuredhash)println("Featured items have changed")
             //modify the saved hash to the current hash
-            templateconfig.hash = template.hashCode()
+            templateConfig.hash = template.hashCode()
             config.featuredhash = config.featured.hashCode()
             Main.saveFile(Main.configFile, gson.toJson(config))
-            Main.saveFile(templateFile, gson.toJson(templateconfig))
+            Main.saveFile(templateFile, gson.toJson(templateConfig))
             println("Compiling All Blogs")
         }
 
 
         //pre-compile the template
-        template = precompile(config, gson, template)
+        template = Main.preCompile(config, templateConfig, gson, template)
 
 
         //Setup the Markdown processor
@@ -100,6 +94,7 @@ class Blog {
         options.setFrom(ParserEmulationProfile.MARKDOWN)
         val p: Parser = Parser.builder(options).build()
         val renderer = HtmlRenderer.builder(options).build()
+
 
         //Go through each found file, and compile it
         println("Getting files and compiling..")
@@ -111,18 +106,29 @@ class Blog {
 
     }
 
-    private fun compileBlog(f:String, gson:Gson, changed:Boolean, config: JsonConfig, parser:Parser, renderer:HtmlRenderer) {
-        var cont: List<String> = Main.readFile(f).split("};")
+    /**
+     * Compile a single Blog
+     *
+     * @param file blog location to compile
+     * @param gson Gson Parser
+     * @param forceCompile compile the blog, even if it hasn't changed
+     * @param config Global Configuration
+     * @param parser Parser
+     * @param renderer Renderer
+     */
+    private fun compileBlog(file:String, gson:Gson, forceCompile:Boolean, config: JsonConfig, parser:Parser, renderer:HtmlRenderer) {
+        //Split the file into the config, and markdown sections
+        var cont: List<String> = Main.readFile(file).split("};")
+
         //account for an uncreated json, by creating a json with 'null' values
         if(cont.size == 1) {
             cont = listOf("{\"title\": \"null\",\"date\":\"null\"", cont[0])
         }
+
         val mdconfig = gson.fromJson(cont[0] + "}", JsonMD::class.java)
 
 
-
         //Parse the date into a timestamp
-
         var timestamp:Long = -1
 
         val formats = Array(3, {""})
@@ -130,12 +136,14 @@ class Blog {
         formats[1] = "MM/dd/yy HH:mm"
         formats[2] = "MM/dd/yy"
 
+        //Try to format the timestamp from the three formats listed above
         for(x in formats) {
             try {
                 timestamp = SimpleDateFormat(x).parse(mdconfig.date).time
                 break
             } catch (e:ParseException) {
                 //do nothing..
+
             }
         }
         if(timestamp.compareTo(-1) == 0) {
@@ -145,35 +153,35 @@ class Blog {
 
         //if the file has not changed since last compile, and we are not compiling everything, skip this file
         if(mdconfig.hash == cont[1].hashCode() && timestamp.compareTo(-1) == 0) {
-            if(!changed)
+            if(!forceCompile)
                 return
         } else {
-            //modify the hash value, and save it to file.
+            //The files have changed, so we need to update the hashes
             mdconfig.hash = cont[1].hashCode()
             mdconfig.unix = timestamp
-            Main.saveFile(f, gson.toJson(mdconfig) + ";" + cont[1])
+            Main.saveFile(file, gson.toJson(mdconfig) + ";" + cont[1])
             //if the json was uncreated/invalid, don't compile it, give an error, and skip the file
             if(mdconfig.title == "null") {
-                println("\tError: invalid json at: $f\n\t\tCreating Json..\n\t\tSkipping $f")
+                println("\tError: invalid json at: $file\n\t\tCreating Json..\n\t\tSkipping $file")
                 return
             }
         }
 
-        println("\tCompiling: " + f)
+        println("\tCompiling: " + file)
 
-        val dir = File(f)
-        val file = File(dir.parentFile.path.replace(config.markdown, config.blog) + "/" + dir.nameWithoutExtension + "/")
+        val dir = File(file)
+        val f = File(dir.parentFile.path.replace(config.markdown, config.blog) + "/" + dir.nameWithoutExtension + "/")
 
         //create the directory, if it couldn't be created, give an error and skip the file
-        if(!file.mkdirs()) {
-            if(!file.exists()) {
-                println("\tUnable to create directory: " + file.path)
+        if(!f.mkdirs()) {
+            if(!f.exists()) {
+                println("\tUnable to create directory: " + f.path)
                 println("\t\tI'm not sure what happened, maybe a folder in the path is a file :/")
                 println("\tskipping..")
                 return
             } else {
-                if(!file.isDirectory) {
-                    println("\tUnable to create directory: " + file.path)
+                if(!f.isDirectory) {
+                    println("\tUnable to create directory: " + f.path)
                     println("\t\tIt is already created as a file!")
                     println("\tskipping..")
                     return
@@ -193,48 +201,12 @@ class Blog {
                 .replace(author, mdconfig.author)
                 .replace(tags, mdconfig.tags)
 
-        Main.saveFile(file.path + "/index.html", text)
+        Main.saveFile(f.path + "/index.html", text)
         println("\tCopying files..")
         mdconfig.files.forEach {
             println("\t\t$it")
-            Files.copy(File(dir.parentFile.path + "/$it").toPath(), File(file.path + "/$it").toPath(), StandardCopyOption.REPLACE_EXISTING)
+            Files.copy(File(dir.parentFile.path + "/$it").toPath(), File(f.path + "/$it").toPath(), StandardCopyOption.REPLACE_EXISTING)
         }
         println("\tDone Copying files")
-    }
-
-    /**
-     * Compile the template with anything that would apply to the entire website
-     */
-    private fun precompile(config: JsonConfig, gson: Gson, t:String):String {
-        var temp = ""
-        var template = t
-        //compile the Featured bar into the template
-        for(i in config.featured.indices) {
-            val conf = gson.fromJson(Main.readFile(config.markdown + "/" + config.featured[i]).split("};")[0]+"}", JsonMD::class.java)
-            val f = File(config.markdown + "/" + config.featured[i])
-            //get the link to the featured post
-            val file = "\\" + f.parentFile.path.replace(config.markdown, config.blog) + "\\" + f.nameWithoutExtension + "\\"
-
-            //Create the html code for the carousel
-            temp +=  (if (i%3 == 0) ("<div class=\"item" + (if(i==0) " active" else "") + "\">\n") else "") +
-                    "\t<div class=\"col-xs-4\">\n\t\t<h5><a href=\"" + file + "\">" + conf.title + "</a></h5>\n\t\t<h6>" +
-                    SimpleDateFormat("MMM d, yyyy").format(Date(conf.unix)) + "</h6>\n\t</div>\n" + (if((i+1)%3 == 0) "</div>\n" else "")
-        }
-        //Add the final div to the carousel if it hasn't already been created
-        if(config.featured.size%3 != 0) {
-            temp += "</div>\n"
-        }
-        //Add the carousel content to the template
-        template = template.replace(carousel, temp)
-
-        //create the proper number of carousel button things for the template
-        temp = ""
-        var i=0
-        while(i< Math.ceil(config.featured.size / 3.0)) {
-            temp += "<li data-target=\"#carousel-featured\" data-slide-to=\"$i\"" + (if(i == 0)" class=\"active\"" else "") +"></li>\n"
-            i++
-        }
-        template = template.replace(carouselInd, temp)
-        return template
     }
 }
--- webcompiler/src/main/kotlin/com/ttocsneb/webcompiler/Main.kt
+++ webcompiler/src/main/kotlin/com/ttocsneb/webcompiler/Main.kt
@@ -2,8 +2,13 @@ package com.ttocsneb.webcompiler
 
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.google.gson.JsonSyntaxException
 import com.ttocsneb.webcompiler.json.JsonConfig
+import com.ttocsneb.webcompiler.json.JsonMD
+import com.ttocsneb.webcompiler.json.JsonTemplate
 import java.io.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 /**
@@ -11,10 +16,20 @@ import java.io.*
  */
 class Main {
 
-    class args {
+    /**
+     * Holds program arguments.
+     */
+    class Args {
         var loadAll = false
         var clean = false
 
+        /**
+         * Process the Arguments from the program
+         *
+         * @param args Arguments given from the main method
+         *
+         * @return Whether the arguments were properly processed
+         */
         fun processargs(args:Array<String>):Boolean {
             var bool = true
             for(arg in args) {
@@ -43,30 +58,34 @@ class Main {
         val configFile:String = "config.json"
 
 
-        @JvmStatic  fun main(args: Array<String>) {
+        @JvmStatic fun main(args: Array<String>) {
 
             val time:Long = System.currentTimeMillis()
 
-            val arg:args = args()
+            //Get the arguments for the program
+            val arg = Args()
             if(!arg.processargs(args))return
 
-            //Create a gson with pretty print enabled.
+            //Create a Gson object with pretty print enabled.
             val gson:Gson = GsonBuilder().setPrettyPrinting().create()
 
             //load the config file
-
             val config = gson.fromJson(readFile(configFile), JsonConfig::class.java)
 
+            //Delete all possible output if the clean argument is enabled.
             if(arg.clean) {
                 delete(File(config.blog))
             }
 
+            //Compile the Blog
             val blog = Blog()
             blog.compile(arg, config)
 
+            //Compile the Main Page
             val main = MainPage()
             main.compile(arg, config)
 
+            //Compile the RSS
             val rss = Rss()
             rss.compile(arg, config)
 
@@ -75,7 +94,16 @@ class Main {
 
         }
 
-        @JvmStatic fun getFiles(directory:String, extension:String):List<String> {
+        /**
+         * Find all files in a folder with a specific extension
+         *
+         * @param directory The location to look in
+         * @param extension What type of file should be found.
+         * @param recursive Whether the search should be recursive
+         *
+         * @return a list of file paths
+         */
+        @JvmStatic fun getFiles(directory: String, extension: String, recursive: Boolean): List<String> {
             val mdfiles:MutableList<String> = mutableListOf()
             //go through each child of the directory
             for(f in File(directory).list()) {
@@ -84,43 +112,161 @@ class Main {
                 if(f.endsWith("." + extension) && File(file).isFile) {
                     //if this is an md file, add it to the list of files
                     mdfiles.add(file)
-                } else if(File(file).isDirectory) {
+                } else if(File(file).isDirectory && recursive) {
                     //if this is a directory, add all files from that directory to the list of files
-                    mdfiles.addAll(getFiles(file, extension))
+                    mdfiles.addAll(getFiles(file, extension, recursive))
                 }
             }
             return mdfiles.toList()
         }
 
         /**
-         * Read a file into a string
+         * Find all files in a folder with a specific extension
+         *
+         * This will recursively search
+         *
+         * @param directory The location to look in
+         * @param extension What type of file should be found.
+         *
+         * @return a list of file paths
          */
-        @JvmStatic fun readFile(directory: String):String {
-            val file = FileReader(File(directory))
-            val str = file.readText()
-            file.close()
+        @JvmStatic fun getFiles(directory: String, extension: String): List<String> {
+            return getFiles(directory, extension, true)
+        }
+
+        /**
+         * Get a specific template
+         *
+         * Specify the directory to find templates, and an ID to look for.
+         *
+         * @param directory Directory to look in
+         * @param templateType JsonTemplate#template ID to look for
+         * @param gson Gson Parser to use
+         *
+         * @return Location of template or null
+         */
+        @JvmStatic fun getTemplate(directory: String, templateType: String, gson: Gson): String? {
+            var templateFile = ""
+
+            //Go through each found json file in the templates folder
+            getFiles(directory, "json").forEach { f ->
+                try {
+                    //try to load the json as a JsonTemplate.
+                    val temp = gson.fromJson(readFile(f), JsonTemplate::class.java)
+
+                    //If the template has the correct ID, return that, else, continue the search
+                    if(temp.template == templateType) {
+                        return f
+                    }
+                } catch (e: JsonSyntaxException) {
+                    println(f + " is not of type JsonTemplate.. Skipping")
+                }
+            }
+
+            return null
+        }
+
+        /**
+         * Compile the site specific items in a template
+         *
+         * @param config Global Configuration
+         * @param templateCfg template Configuration
+         * @param gson Gson parser
+         * @param template Template to compile
+         *
+         * @return partially compiled Template
+         */
+        @JvmStatic fun preCompile(config: JsonConfig, templateCfg: JsonTemplate, gson: Gson, template:String): String {
+            var temp = ""
+            var t = template
+
+            var carousel: String = ""
+            var carouselInd: String = ""
+
+            //Load the tags
+            templateCfg.items.forEach { item ->
+                when(item.name) {
+                    "carousel" -> carousel = item.value
+                    "carouselInd" -> carouselInd = item.value
+                }
+            }
+
+
+            //compile the Featured bar into the template
+            for(i in config.featured.indices) {
+                val conf = gson.fromJson(Main.readFile(config.markdown + "/" + config.featured[i]).split("};")[0]+"}", JsonMD::class.java)
+                val f = File(config.markdown + "/" + config.featured[i])
+                //get the link to the featured post
+                val file = "\\" + f.parentFile.path.replace(config.markdown, config.blog) + "\\" + f.nameWithoutExtension + "\\"
+
+                //Create the html code for the carousel
+                temp +=  (if (i%3 == 0) ("<div class=\"item" + (if(i==0) " active" else "") + "\">\n") else "") +
+                        "\t<div class=\"col-xs-4\">\n\t\t<h5><a href=\"" + file + "\">" + conf.title + "</a></h5>\n\t\t<h6>" +
+                        SimpleDateFormat("MMM d, yyyy").format(Date(conf.unix)) + "</h6>\n\t</div>\n" + (if((i+1)%3 == 0) "</div>\n" else "")
+            }
+            //Add the final div to the carousel if it hasn't already been created
+            if(config.featured.size%3 != 0) {
+                temp += "</div>\n"
+            }
+            //Load the featured items into the template
+            t = t.replace(carousel, temp)
+
+            //create the proper number of carousel button things for the template
+            temp = ""
+            var i=0
+            while(i< Math.ceil(config.featured.size / 3.0)) {
+                temp += "<li data-target=\"#carousel-featured\" data-slide-to=\"$i\"" + (if(i == 0)" class=\"active\"" else "") +"></li>\n"
+                i++
+            }
+            t = t.replace(carouselInd, temp)
+
+
+            return t
+        }
+
+        /**
+         * Read a file into a string
+         *
+         * @param file The file to read
+         *
+         * @return Contents of the file
+         */
+        @JvmStatic fun readFile(file: String): String {
+            val f = FileReader(File(file))
+            val str = f.readText()
+            f.close()
             return str
         }
 
         /**
          * Save a file.
+         *
+         * @param file file to write
+         * @param content what to write to the file
          */
-        @JvmStatic fun saveFile(directory: String, content: String) {
-            val fw = FileWriter(directory)
+        @JvmStatic fun saveFile(file: String, content: String) {
+            val fw = FileWriter(file)
             val bw = BufferedWriter(fw)
             bw.write(content)
             bw.close()
             fw.close()
         }
 
+        /**
+         * Delete a file
+         *
+         * @param file file to delete
+         *
+         * @throws IOException
+         */
         @Throws(IOException::class)
-        @JvmStatic fun delete(f: File) {
-            if (f.isDirectory) {
-                for (c in f.listFiles()!!)
+        @JvmStatic fun delete(file: File) {
+            if (file.isDirectory) {
+                for (c in file.listFiles()!!)
                     delete(c)
             }
-            if (!f.delete())
-                throw FileNotFoundException("Failed to delete file: " + f)
+            if (!file.delete())
+                throw FileNotFoundException("Failed to delete file: " + file)
         }
     }
 
